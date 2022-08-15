fad757b0f7cb3d52ecf8f53df8affe026357bf5f
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-08-15 09:37:45 -0300

    style: change style do styled.js file

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
 
-import styled from 'styled-components';
 import trunfoLogo from '../../images/logo_trunfo_game.svg';
 
-const Wrapper = styled.div`
-  display: flex;
-  height: 6rem;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  color: #2a2a2a;
-  background-color: #f95f18;
-
-  img {
-    width: 6rem;
-  }
-`;
+import * as S from './styled';
 
 class Header extends Component {
   render() {
     return (
-      <Wrapper>
+      <S.Wrapper>
         <img src={ trunfoLogo } alt="Trunfo Logo" />
         <h1>Jogo Trunfo - Heroínas Brasileiras</h1>
-      </Wrapper>
+      </S.Wrapper>
     );
   }
 }
---
+++ src/components/Header/styled.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const Wrapper = styled.header`
+  display: flex;
+  height: 6rem;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #2a2a2a;
+  background-color: #f95f18;
+
+  img {
+    width: 6rem;
+  }
+`;
