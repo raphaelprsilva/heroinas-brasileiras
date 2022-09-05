918a8ebde4819a43b6b576b36e8027d1d6611da2
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-09-05 09:42:20 -0300

    style: change button style

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -2,7 +2,8 @@
 import styled from 'styled-components';
 
 export const ButtonWrapper = styled.button`
-  background-color: #fa814a;
+  width: 80px;
+  background-color: #FF1789;
   padding: 0.5rem 0;
   border-radius: 5px;
   color: #ffffff;
@@ -10,7 +11,6 @@ export const ButtonWrapper = styled.button`
   transition: background-color 0.5s ease;
 
   :hover {
-    background-color: #f95f18;
     cursor: pointer;
   }
 
